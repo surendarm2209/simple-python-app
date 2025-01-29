82a258c0ae023bc252dd1eeddbea2ab6370b6a5f
surendarm2209 <surendarm2209@gmail.com>
2025-01-29 18:11:39 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull surendarm2207/simple-python-flask-app
+docker pull surendarm2207/simple-python-flask-appone
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 surendarm2207/simple-python-flask-app
+docker run -d -p 5000:5000 surendarm2207/simple-python-flask-appone
